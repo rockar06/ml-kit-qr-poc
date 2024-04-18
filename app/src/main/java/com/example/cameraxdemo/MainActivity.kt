57f9df45bc0c11a5e1f9d569c71f5ae7be47e082
Armando Ruiz <aranruiz13@gmail.com>
2024-04-17 18:01:40 -0600

    Add basic stuff

---
+++ app/src/main/java/com/example/cameraxdemo/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.cameraxdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.example.cameraxdemo.ui.CameraScreen
+import com.example.cameraxdemo.ui.theme.CameraXDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                println("CAMERA: Allowed")
+            }
+
+            else -> {
+                cameraPermissionRequest.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+
+        setContent {
+            CameraXDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraScreen()
+                }
+            }
+        }
+    }
+
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Implement camera related  code
+            } else {
+                // Camera permission denied
+            }
+
+        }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CameraXDemoTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/cameraxdemo/ui/CameraScreen.kt
@@ -0,0 +1,8 @@
+package com.example.cameraxdemo.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraScreen() {
+
+}
